737bd849bc3715e89eb90befe1a345b9963ed199
M S <syedmashoodahmedshah@hotmail.com>
2019-05-10 15:01:57 +0100

    Add project files.

---
+++ SavingsTracker/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace SavingsTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_yearlysavings = new System.Windows.Forms.Label();
+            this.tb_savingsyearly = new System.Windows.Forms.TextBox();
+            this.lbl_savingsgoal = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lbl_monthlysavings = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_savingsaccount = new System.Windows.Forms.Label();
+            this.tb_savingsaccount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_sg1 = new System.Windows.Forms.TextBox();
+            this.lbl_sg1 = new System.Windows.Forms.Label();
+            this.tb_income = new System.Windows.Forms.TextBox();
+            this.tb_expenses = new System.Windows.Forms.TextBox();
+            this.lbl_income = new System.Windows.Forms.Label();
+            this.lbl_expenses = new System.Windows.Forms.Label();
+            this.tb_sg2 = new System.Windows.Forms.TextBox();
+            this.lbl_sg2 = new System.Windows.Forms.Label();
+            this.lbl_sg4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lbl_sg3 = new System.Windows.Forms.Label();
+            this.tb_sg3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_yearlysavings
+            // 
+            this.lbl_yearlysavings.AutoSize = true;
+            this.lbl_yearlysavings.Location = new System.Drawing.Point(9, 201);
+            this.lbl_yearlysavings.Name = "lbl_yearlysavings";
+            this.lbl_yearlysavings.Size = new System.Drawing.Size(75, 13);
+            this.lbl_yearlysavings.TabIndex = 51;
+            this.lbl_yearlysavings.Text = "Yearly savings";
+            // 
+            // tb_savingsyearly
+            // 
+            this.tb_savingsyearly.Location = new System.Drawing.Point(196, 198);
+            this.tb_savingsyearly.Name = "tb_savingsyearly";
+            this.tb_savingsyearly.Size = new System.Drawing.Size(100, 20);
+            this.tb_savingsyearly.TabIndex = 50;
+            this.tb_savingsyearly.Text = "0";
+            // 
+            // lbl_savingsgoal
+            // 
+            this.lbl_savingsgoal.AutoSize = true;
+            this.lbl_savingsgoal.Location = new System.Drawing.Point(12, 101);
+            this.lbl_savingsgoal.Name = "lbl_savingsgoal";
+            this.lbl_savingsgoal.Size = new System.Drawing.Size(68, 13);
+            this.lbl_savingsgoal.TabIndex = 49;
+            this.lbl_savingsgoal.Text = "Savings goal";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(199, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 48;
+            this.textBox2.Text = "0";
+            // 
+            // lbl_monthlysavings
+            // 
+            this.lbl_monthlysavings.AutoSize = true;
+            this.lbl_monthlysavings.Location = new System.Drawing.Point(12, 127);
+            this.lbl_monthlysavings.Name = "lbl_monthlysavings";
+            this.lbl_monthlysavings.Size = new System.Drawing.Size(83, 13);
+            this.lbl_monthlysavings.TabIndex = 47;
+            this.lbl_monthlysavings.Text = "Monthly savings";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(199, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 46;
+            this.textBox1.Text = "0";
+            // 
+            // lbl_savingsaccount
+            // 
+            this.lbl_savingsaccount.AutoSize = true;
+            this.lbl_savingsaccount.Location = new System.Drawing.Point(12, 22);
+            this.lbl_savingsaccount.Name = "lbl_savingsaccount";
+            this.lbl_savingsaccount.Size = new System.Drawing.Size(87, 13);
+            this.lbl_savingsaccount.TabIndex = 45;
+            this.lbl_savingsaccount.Text = "Savings account";
+            // 
+            // tb_savingsaccount
+            // 
+            this.tb_savingsaccount.Location = new System.Drawing.Point(199, 15);
+            this.tb_savingsaccount.Name = "tb_savingsaccount";
+            this.tb_savingsaccount.Size = new System.Drawing.Size(100, 20);
+            this.tb_savingsaccount.TabIndex = 44;
+            this.tb_savingsaccount.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(283, 13);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "______________________________________________";
+            // 
+            // tb_sg1
+            // 
+            this.tb_sg1.Location = new System.Drawing.Point(196, 224);
+            this.tb_sg1.Name = "tb_sg1";
+            this.tb_sg1.Size = new System.Drawing.Size(100, 20);
+            this.tb_sg1.TabIndex = 53;
+            this.tb_sg1.Text = "0";
+            // 
+            // lbl_sg1
+            // 
+            this.lbl_sg1.AutoSize = true;
+            this.lbl_sg1.Location = new System.Drawing.Point(12, 230);
+            this.lbl_sg1.Name = "lbl_sg1";
+            this.lbl_sg1.Size = new System.Drawing.Size(79, 13);
+            this.lbl_sg1.TabIndex = 54;
+            this.lbl_sg1.Text = "Saving goal #1";
+            // 
+            // tb_income
+            // 
+            this.tb_income.Location = new System.Drawing.Point(199, 41);
+            this.tb_income.Name = "tb_income";
+            this.tb_income.Size = new System.Drawing.Size(100, 20);
+            this.tb_income.TabIndex = 55;
+            this.tb_income.Text = "0";
+            // 
+            // tb_expenses
+            // 
+            this.tb_expenses.Location = new System.Drawing.Point(199, 68);
+            this.tb_expenses.Name = "tb_expenses";
+            this.tb_expenses.Size = new System.Drawing.Size(100, 20);
+            this.tb_expenses.TabIndex = 56;
+            this.tb_expenses.Text = "0";
+            // 
+            // lbl_income
+            // 
+            this.lbl_income.AutoSize = true;
+            this.lbl_income.Location = new System.Drawing.Point(12, 48);
+            this.lbl_income.Name = "lbl_income";
+            this.lbl_income.Size = new System.Drawing.Size(42, 13);
+            this.lbl_income.TabIndex = 57;
+            this.lbl_income.Text = "Income";
+            // 
+            // lbl_expenses
+            // 
+            this.lbl_expenses.AutoSize = true;
+            this.lbl_expenses.Location = new System.Drawing.Point(12, 75);
+            this.lbl_expenses.Name = "lbl_expenses";
+            this.lbl_expenses.Size = new System.Drawing.Size(53, 13);
+            this.lbl_expenses.TabIndex = 58;
+            this.lbl_expenses.Text = "Expenses";
+            // 
+            // tb_sg2
+            // 
+            this.tb_sg2.Location = new System.Drawing.Point(196, 251);
+            this.tb_sg2.Name = "tb_sg2";
+            this.tb_sg2.Size = new System.Drawing.Size(100, 20);
+            this.tb_sg2.TabIndex = 59;
+            this.tb_sg2.Text = "0";
+            // 
+            // lbl_sg2
+            // 
+            this.lbl_sg2.AutoSize = true;
+            this.lbl_sg2.Location = new System.Drawing.Point(12, 258);
+            this.lbl_sg2.Name = "lbl_sg2";
+            this.lbl_sg2.Size = new System.Drawing.Size(79, 13);
+            this.lbl_sg2.TabIndex = 60;
+            this.lbl_sg2.Text = "Saving goal #2";
+            // 
+            // lbl_sg4
+            // 
+            this.lbl_sg4.AutoSize = true;
+            this.lbl_sg4.Location = new System.Drawing.Point(12, 311);
+            this.lbl_sg4.Name = "lbl_sg4";
+            this.lbl_sg4.Size = new System.Drawing.Size(79, 13);
+            this.lbl_sg4.TabIndex = 64;
+            this.lbl_sg4.Text = "Saving goal #4";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(196, 304);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 63;
+            this.textBox3.Text = "0";
+            // 
+            // lbl_sg3
+            // 
+            this.lbl_sg3.AutoSize = true;
+            this.lbl_sg3.Location = new System.Drawing.Point(12, 284);
+            this.lbl_sg3.Name = "lbl_sg3";
+            this.lbl_sg3.Size = new System.Drawing.Size(79, 13);
+            this.lbl_sg3.TabIndex = 62;
+            this.lbl_sg3.Text = "Saving goal #3";
+            // 
+            // tb_sg3
+            // 
+            this.tb_sg3.Location = new System.Drawing.Point(196, 277);
+            this.tb_sg3.Name = "tb_sg3";
+            this.tb_sg3.Size = new System.Drawing.Size(100, 20);
+            this.tb_sg3.TabIndex = 61;
+            this.tb_sg3.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 386);
+            this.Controls.Add(this.lbl_sg4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lbl_sg3);
+            this.Controls.Add(this.tb_sg3);
+            this.Controls.Add(this.lbl_sg2);
+            this.Controls.Add(this.tb_sg2);
+            this.Controls.Add(this.lbl_expenses);
+            this.Controls.Add(this.lbl_income);
+            this.Controls.Add(this.tb_expenses);
+            this.Controls.Add(this.tb_income);
+            this.Controls.Add(this.lbl_sg1);
+            this.Controls.Add(this.tb_sg1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_yearlysavings);
+            this.Controls.Add(this.tb_savingsyearly);
+            this.Controls.Add(this.lbl_savingsgoal);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbl_monthlysavings);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_savingsaccount);
+            this.Controls.Add(this.tb_savingsaccount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_yearlysavings;
+        private System.Windows.Forms.TextBox tb_savingsyearly;
+        private System.Windows.Forms.Label lbl_savingsgoal;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lbl_monthlysavings;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_savingsaccount;
+        private System.Windows.Forms.TextBox tb_savingsaccount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_sg1;
+        private System.Windows.Forms.Label lbl_sg1;
+        private System.Windows.Forms.TextBox tb_income;
+        private System.Windows.Forms.TextBox tb_expenses;
+        private System.Windows.Forms.Label lbl_income;
+        private System.Windows.Forms.Label lbl_expenses;
+        private System.Windows.Forms.TextBox tb_sg2;
+        private System.Windows.Forms.Label lbl_sg2;
+        private System.Windows.Forms.Label lbl_sg4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lbl_sg3;
+        private System.Windows.Forms.TextBox tb_sg3;
+    }
+}
+
